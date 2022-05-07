c160dcdf2aaf7b3959ae167c23a99aedc4ec55d1
Matheus51bento <matheus51bento@gmail.com>
2022-05-07 15:23:33 -0300

    urls

--- ElectSis/eleicao/urls.py
+++ ElectSis/eleicao/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
 
-    path('', index, name="Inicio")
+    path('', index, name="inicio"),
+    path('cadastro/eleicoes/', cad_eleicao, name="cadastro eleicao"),
+    path('cadastro/candidato', cad_candidadto, name="cadastro candidato")
 
 ]
--- ElectSis/eleicao/views.py
+++ ElectSis/eleicao/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-
     return render(request, "menu.html")
+
+def cad_eleicao(request):
+    return render(request, "cadastro_eleicao.html")
+
+def cad_candidadto(request):
+    return render(request, "cadastro_candidato.html")
